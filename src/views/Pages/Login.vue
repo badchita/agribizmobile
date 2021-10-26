c8fc71aea92af9f73ec28b9913964a704040f6dc
badchita <47920377+badchita@users.noreply.github.com>
2021-10-26 21:58:45 +0800

    onChange on Login.vue

--- src/views/Pages/Login.vue
+++ src/views/Pages/Login.vue
@@ -14,13 +14,13 @@
                     <div class="ion-margin-end">
                         <ion-icon color="medium" name="person-outline" />
                     </div>
-                    <ion-input placeholder="Email" />
+                    <ion-input v-model="auth.email" placeholder="Email" @IonChange="onChangeEmailPassInput" />
                 </ion-item>
                 <ion-item>
-                    <div class="ion-margin-end">
+                    <div style="margin-right: 8px;">
                         <ion-icon color="medium" name="lock-closed-outline" />
                     </div>
-                    <ion-input placeholder="Password" :type="passwordVisibility" />
+                    <ion-input v-model="auth.password" placeholder="Password" :type="passwordVisibility" @IonChange="onChangeEmailPassInput" />
                     <ion-buttons>
                         <ion-button v-if="passwordVisibility === 'password'" @click="onClickIconEyeOff">
                             <ion-icon color="medium" name="eye-off" />
@@ -33,7 +33,8 @@
                         <router-link to="">Forgot?</router-link>
                     </ion-label>
                 </ion-item>
-                <ion-button class="login-active-button" expand="full">Log In</ion-button>
+                <ion-button class="login-active-button" expand="full" :disabled="loginButtonDisabled">Log In
+                </ion-button>
                 <div class="ion-margin-top">
                     <ion-label>
                         <router-link to="">Sign up</router-link>
@@ -56,11 +57,26 @@
 
 <script>
     import {
+        reactive,
         ref
     } from '@vue/reactivity'
+    import {
+        onMounted
+    } from '@vue/runtime-core'
+
     export default {
         setup() {
+            onMounted(() => {
+                onChangeEmailPassInput()
+            })
+            let auth = reactive({
+                email: '',
+                password: '',
+                password_confirmation: '',
+            })
+
             let passwordVisibility = ref('password')
+            let loginButtonDisabled = ref(true)
 
             function onClickIconEyeOff() {
                 passwordVisibility.value = 'text'
@@ -69,10 +85,21 @@
             function onClickIconEye() {
                 passwordVisibility.value = 'password'
             }
+
+            function onChangeEmailPassInput() {
+                if (auth.email === '' || auth.password === '') {
+                    loginButtonDisabled.value = true
+                } else {
+                    loginButtonDisabled.value = false
+                }
+            }
             return {
                 onClickIconEyeOff,
                 onClickIconEye,
-                passwordVisibility
+                passwordVisibility,
+                auth,
+                onChangeEmailPassInput,
+                loginButtonDisabled
             }
         }
     }
